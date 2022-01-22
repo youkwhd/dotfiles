b8350c6fe56d69c6292fa6b191c9266665017903
youkwhd <youkwhd@gmail.com>
2022-01-22 22:12:13 +0700

    add nerd font to integrate with nvim dev icons

--- .config/suckless/st/config.h
+++ .config/suckless/st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Dina TTF:size=10:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true", "FiraCode Nerd Font:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
